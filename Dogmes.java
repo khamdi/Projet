4e6b711dcd9bcc9f9127264cd0c67e6baf58977b
khamdi <kaki131@hotmail.fr>
2016-12-07 18:23:37 +0100

    changement

--- Dogmes.java
+++
@@ -1,9 +0,0 @@
-package fr.utt.a16.lo02.projet;
-
-public enum Dogmes {
-	Nature,
-	Chaos,
-	Humain,
-	Mystique,
-	Symnoles
-}
--- Origine.java
+++
@@ -1,9 +0,0 @@
-package fr.utt.a16.lo02.projet;
-
-public enum Origine {
-	Jour,
-	Nuit,
-	Neant,
-	Aube,
-	Crepuscule
-}
